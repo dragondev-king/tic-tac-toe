60f854678d40906f9e190dcf359b1ef75645a5f5
dragondev-king <dragondev1017@gmail.com>
2022-01-07 09:48:38 -0800

    Implement taking turns

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React from 'react'
-
-import logo from './logo.svg';
 import './App.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props)  {
+  return (
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
   handleClick(i) {
     const squares = this.state.squares.slice()
-    squares[i] = 'X'
-    this.setState({squares: squares})
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({squares: squares, xIsNext: !this.state.xIsNext})
   }
   renderSquare(i) {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ?  'X': 'O'}`;
 
     return (
       <div>
